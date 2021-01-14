a8578fa4608184df890f49b1543123c66da67b0d
Furkan Ozturk <furkanozturk1988@gmail.com>
2021-01-14 17:31:10 -0600

    furkan2

---
+++ src/test/java/tests/Another.java
@@ -0,0 +1,27 @@
+package tests;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import utilities.Driver;
+
+public class Another extends TestBase {
+	
+
+	@Test
+	public void test1() {
+		
+			
+		driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+//      
+		driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+		driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+		driver.findElement(By.name("ctl00$MainContent$login_button")).click();
+        assertTrue(Driver.getDriver().getTitle().equals("Web Orders"));
+        
+//
+	}
+
+}
